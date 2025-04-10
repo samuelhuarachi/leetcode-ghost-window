2a3bbe5107342fb4d5d90ee2a64690335531ce60
Samuel Gomes <samuel.huarachi@gmail.com>
2025-04-09 22:11:49 -0300

    in progress - now ghost windows work on win11, electron 33 versino

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
 
+// We are using Node.js 20.18.3, Chromium 130.0.6723.191, and Electron 33.4.9.
+
+
 function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    backgroundThrottling : true,
+    hasShadow: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
